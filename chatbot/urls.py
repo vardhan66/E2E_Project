58e43449f88ebf35f6b0e7ff38558bf5884d0f1c
BhargavBunty <bhargavbunty@059gmail.com>
2024-06-15 22:29:04 +0530

    clean redundancies

--- chatbot/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('',views.index,name='index'),
-    path('specific',views.specific,name='specific'),
-    path('getResponse',views.getResponse,name='getResponse')
-]
--- forgotpassword/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path, include
-from . import views
-
-urlpatterns = [
-    path('forgotpassword/', views.forgotpassword, name='forgotpassword'),
-    path('login/', include('login.urls')),  
-]
